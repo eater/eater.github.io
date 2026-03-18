e4e02f6cf9f9269f81d86080197ac2294ea5e587
eater <biz@pote.com>
2026-03-18 18:12:53 -0400

    added PWA manifest and icons

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'rubbersoul-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install the service worker and cache the files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Intercept network requests and serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
